d6dcad5691a4c5aec720983062581d61cdd0ec73
zohaib khan <zohaibkhan1162002@gmail>
2022-07-05 08:27:56 -0700

    Installed react icons

--- src/Components/quote.js
+++ src/Components/quote.js
@@ -3,10 +3,10 @@ import React from "react";
 const Quote = () => {
   return <div className="quote-container">
     <h2 className="quote">Why are you dying to live when you are living to die </h2>
-    <h3 className="author">Tupac Shakur</h3>
+    <h3 className="author">-- Tupac Shakur</h3>
     <div className="btns-container">
-        <button className="tweet-btn">Tweet</button>
-        <button className="next-btn">Next Quote</button>
+        <button className="tweet-btn btn">Tweet</button>
+        <button className="next-btn btn">Next Quote</button>
     </div>
   </div>;
 };
