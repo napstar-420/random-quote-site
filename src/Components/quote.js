f06fe1dc00308971fe0db3f2b2d764c982e6a14e
zohaib khan <zohaibkhan1162002@gmail>
2022-07-05 09:13:39 -0700

    Added style

--- src/Components/quote.js
+++ src/Components/quote.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
 
 const Quote = () => {
   return <div className="quote-container">
-    <h2 className="quote">Why are you dying to live when you are living to die </h2>
-    <h3 className="author">-- Tupac Shakur</h3>
+    <h2 className="quote"><FaQuoteLeft style={{marginRight: '0.8rem'}}/>Why are you dying to live when you are living to die </h2>
+    <h3 className="author">_Tupac Shakur</h3>
     <div className="btns-container">
-        <button className="tweet-btn btn">Tweet</button>
+        <button className="tweet-btn btn"><FaTwitter className="tweet-icon"/>Tweet</button>
         <button className="next-btn btn">Next Quote</button>
     </div>
   </div>;
