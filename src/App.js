b09902302c918dfa9b2af72393fdd32cfc51f60b
zohaib khan <zohaibkhan1162002@gmail>
2022-07-05 09:42:11 -0700

    Added fetching functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Quote from "./Components/quote.js";
 import Navbar from "./Components/navbar.js";
 import "./index.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  const fetchQuotes = async () => {
+    let response = await fetch("https://type.fit/api/quotes");
+    let data = await response.json();
+    setQuotes(data);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
-      <Quote />
+      <Quote quotes={quotes} />
     </div>
   );
 }
--- src/Components/quote.js
+++ src/Components/quote.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
 
-const Quote = () => {
+const Quote = ({quotes}) => {
+    console.log(quotes);
   return <div className="quote-container">
-    <h2 className="quote"><FaQuoteLeft style={{marginRight: '0.8rem'}}/>Why are you dying to live when you are living to die </h2>
-    <h3 className="author">_Tupac Shakur</h3>
+    <h2 className="quote"><FaQuoteLeft style={{marginRight: '0.8rem'}}/>{quotes[0].text}</h2>
+    <h3 className="author">_{quotes[0].author}</h3>
     <div className="btns-container">
         <button className="tweet-btn btn"><FaTwitter className="tweet-icon"/>Tweet</button>
         <button className="next-btn btn">Next Quote</button>
