f5748cccf1468079f94ea88960875e63a01b2405
zohaib khan <zohaibkhan1162002@gmail>
2022-07-05 08:06:48 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Quote from "./Components/quote.js";
-
+import "./index.css";
 function App() {
   return (
     <div className="App">
--- src/Components/quote.js
+++ src/Components/quote.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Quote = () => {
-  return <div>Quote</div>;
+  return <div className="quote-container">
+    <h2 className="quote">Why are you dying to live when you are living to die </h2>
+    <h3 className="author">Tupac Shakur</h3>
+    <div className="btns-container">
+        <button className="tweet-btn">Tweet</button>
+        <button className="next-btn">Next Quote</button>
+    </div>
+  </div>;
 };
 
 export default Quote;
